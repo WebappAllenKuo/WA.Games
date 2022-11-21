57b912d643146b1f5c709ded4b32f81bc079a5ba
LillianLin <a888gogogo@yahoo.com.tw>
2022-11-21 17:21:50 +0800

    Initial commit

---
+++ WA.Games.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace WA.Games.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
